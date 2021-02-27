39dda6eba047fbcc08b7a4d8c44c22393b06dd5c
Froz10 <ivan.frozby@gmail.com>
2021-02-27 16:54:57 +0600

    Create DB Redis

---
+++ db/storage.rb
@@ -0,0 +1,17 @@
+module API
+  class Storage
+
+    def initialize
+      @redis = Redis.new
+    end
+
+    def write_session(key, time, value)
+      value.each { |value| @redis.zadd(key, time, value)}
+    end
+
+    def find_session(key, min, max)  
+      @redis.zrangebyscore(key, min, max)
+    end
+
+  end
+end
